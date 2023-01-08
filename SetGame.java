c72e6f4099cf2a0b644dc49f599d16c5f116edec
Jay Coskey <jay.coskey@gmail.com>
2023-01-07 23:18:45 -0800

    Make user entry of moves more robust with regexes. Track number of Sets found.

--- SetGame.java
+++ SetGame.java
@@ -1,17 +1,26 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
+
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Optional;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 class SetGame {
-    public static boolean isHelpEnabled = true;   // Show user a Set if one is present that they cannot find.
-    public static boolean isMercyEnabled = false;  // TODO: End game if deck is empty and no more Sets are present.
+    // Show user a Set if one is present that they cannot find.
+    public static boolean isHelpEnabled = true;
+
+    // TODO: End game if deck is empty and no more Sets are present.
+    public static boolean isMercyEnabled = false;
 
     // TODO: Refactor into smaller pieces.
     public static void main(String [] args) throws EmptyException, IOException {
+        int numSetsFound = 0;
+
         // Play the game
         for (;;) {
             SetDeck deck = new SetDeck();
@@ -30,10 +39,10 @@ class SetGame {
             assert(tableCards.size() == SetDeck.BASE_OUTLAY_COUNT);
 
             for (;;) {  // User looking for set
-                deck.printSize();
+                printGameStats(numSetsFound, tableCards.size(), deck.size());
                 tableCards.printNumeric();
 
-                Optional<int[]> optUserResponse = queryUserSet();
+                Optional<int[]> optUserResponse = queryUserSet(tableCards.size());
                 if (optUserResponse.isPresent()) {
                     // User claims to have found a set
                     int[] userSetIndxs = optUserResponse.get();
@@ -44,6 +53,7 @@ class SetGame {
                     if (isSet(cards)) {
                         // User found a set
                         System.out.println("*** You found a set!");
+                        numSetsFound++;
                         tableCards.removeThree(userSetIndxs);
                         if (tableCards.isEmpty()) {
                             System.out.println("You cleared the board. Congratulations!!");
@@ -133,6 +143,19 @@ class SetGame {
         return true;
     }
 
+    public static void printGameStats(int numSetsFound, int tableCardCount, int deckCardCount) {
+        String setCountStr = numSetsFound == 0 ? "No Sets"
+                                 : ((numSetsFound == 1) ? "One Set"
+                                 : String.format("%d Sets", numSetsFound));
+        String tableCountStr = String.format("%d cards", tableCardCount);
+        String deckCountStr = tableCardCount == 0 ? "No cards"
+                                 : String.format("%d cards", deckCardCount);
+
+        String outStr = String.format("%s found. %s on the table. %s in the deck.",
+                                          setCountStr, tableCountStr, deckCountStr);
+        System.out.println(outStr);
+    }
+
     public static void printSet(SetCardCollection cards, int[] indxs) {
         System.out.println(
             "Cards #" + String.valueOf(indxs[0])
@@ -156,17 +179,36 @@ class SetGame {
         return true;
     }
 
-    public static Optional<int[]> queryUserSet() throws IOException {
+    public static Optional<int[]> queryUserSet(int tableCardCount) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        Pattern regex = Pattern.compile("^\\s*\\d+\\s+\\d+\\s+\\d+\\s*$");
 
         for (;;) {
-            System.out.print("\nEnter three card numbers (or <RETURN> for no set found): ");
+            System.out.print("\nEnter three card #numbers (or <RETURN> for no Set found): ");
             String userInput = in.readLine().trim();
             if (userInput.isEmpty()) {
                 return Optional.empty();  // User did not find set
             }
-            // TODO: Make robust against input errors, both in format and range.
-            int[] cardIndxs = Arrays.stream(userInput.split(" ")).mapToInt(Integer::parseInt).toArray();
+            Matcher matcher = regex.matcher(userInput);
+            if (!matcher.find()) {
+                System.out.println("Error: Did not recognize input format.");
+                continue;
+            }
+            int[] cardIndxs = Arrays.stream(userInput.split(" "))
+                                  .filter(s -> s.length() > 0)
+                                  .mapToInt(Integer::parseInt)
+                                  .toArray();
+            boolean areIndxsValid = true;
+            for (int cardIndx : cardIndxs) {
+                if (cardIndx < 0 || cardIndx >= tableCardCount) {
+                    System.out.format("Error: Cards #%d is out of range (0 to %d)",
+                        cardIndx, tableCardCount - 1);
+                    areIndxsValid = false;
+                }
+            }
+            if (!areIndxsValid) {
+                continue;
+            }
             return Optional.ofNullable(cardIndxs);
         }
         // NOT_REACHED
